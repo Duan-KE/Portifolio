2cd3d5617a07376416dfe47e0b7abb4c8a758f2e
Collince Amiani <duanixmedia@gmail.com>
2024-10-01 14:19:21 +0000

    several updates

--- script.js
+++ script.js
@@ -4,23 +4,21 @@ let selections=document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
 
 
-window.onscroll=() =>{
-    selections.forEach(src =>{
-        let top=window.scrollY;
-        let offset = sec.offsetTop = 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// window.onscroll=() =>{
+//     selections.forEach(src =>{
+//         let top=window.scrollY;
+//         let offset = sec.offsetTop = 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a 
-                    [href*=' + id + ' ]').classList.add
-                    ('active')
-            })
-        }
-    })
-}
+//         if(top >= offset && top < offset + height){
+//             navlinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a').classList.add('active')
+//             })
+//         }
+//     })
+// }
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
